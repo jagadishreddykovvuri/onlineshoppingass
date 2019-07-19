07afde0f5daa6f7503f54cc324132d1a0260b0b3
kovvuri jagadish reddy <jagadishreddykovvuri@gmail.com>
2019-07-19 09:13:38 +0530

    each product show component done

--- src/OnlineShoppingPage/ProductShowCase/Product/index.js
+++ src/OnlineShoppingPage/ProductShowCase/Product/index.js
@@ -8,6 +8,8 @@ class Product extends Component {
           <div className="product-image">
             <div class="free-shipping">Free Shipping</div>
           </div>
+          <h2 className="price">Title</h2>
+          <p className="price">price</p>
           <button className="add-to-cart">Add To Cart</button>
         </div>
       </>
