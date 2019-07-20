06f521a87fd6c1176bcc1a5f96c46c5113c28941
kovvuri jagadish reddy <jagadishreddykovvuri@gmail.com>
2019-07-20 09:02:45 +0530

    checklist component done

--- src/OnlineShoppingPage/Cart/CartItems/CartProducts/index.js
+++ src/OnlineShoppingPage/Cart/CartItems/CartProducts/index.js
@@ -1,25 +1,35 @@
 import React, { Component } from "react";
+import { products } from "../../../../constant";
 import "./styles.css";
+import { observer } from "mobx-react";
+@observer
 class CartProduct extends Component {
+  findProductDetails = () => {
+    for (let i = 0; i < products.length; i++) {
+      if (products[i].id === this.props.cartItem.id) {
+        return i;
+      }
+    }
+  };
   render() {
+    let index = this.findProductDetails();
     return (
       <div className="each-cart-item">
         <div>
-          <img
-            className="product-photo"
-            src="https://react-shopping-cart-67954.firebaseapp.com/static/media/18532669286405344_1.d78e2790.jpg"
-          />
+          <img className="product-photo" src={products[index].image} />
         </div>
         <div className="product-details">
-          <h2 className="title-color">title</h2>
+          <h2 className="title-color">{products[index].title}</h2>
           <div className="detailstyle">
-            <p>size|model</p>
-            <p>quantity</p>
+            <p>
+              {products[index].availableSizes[0]}|{products[index].style}
+            </p>
+            <p>{this.props.cartItem.quantity}</p>
           </div>
         </div>
         <div className="delete-with-price">
           <img className="delete-icon" src="/assets/error.png" />
-          <p>price</p>
+          <p className="price">price</p>
         </div>
       </div>
     );
--- src/OnlineShoppingPage/Cart/CartItems/index.js
+++ src/OnlineShoppingPage/Cart/CartItems/index.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 import CartProduct from "./CartProducts";
+import { cartStore } from "../../../Instance";
 import "./styles.css";
+import { observer } from "mobx-react";
+@observer
 class CartItem extends Component {
+  cartProducts = () => {
+    if (cartStore.cartProducts.length > 0) {
+      return cartStore.cartProducts.map(cartItem => (
+        <CartProduct cartItem={cartItem} />
+      ));
+    }
+  };
   render() {
     return (
       <>
-        <div className="cart-products">
-          <CartProduct />
-        </div>
+        <div className="cart-products">{this.cartProducts()}</div>
       </>
     );
   }
